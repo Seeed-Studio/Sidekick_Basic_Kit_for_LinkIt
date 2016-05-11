0a9db3a01993ba0e6032752876dd66c7491deaf5
Lowaichung <luweicong@seeedstudio.com>
2016-05-11 16:44:43 +0800

    Update L4_Control_LEDs.ino

--- Basic/L4_Control_LEDs/L4_Control_LEDs.ino
+++ Basic/L4_Control_LEDs/L4_Control_LEDs.ino
@@ -10,8 +10,8 @@
  * Led1 connect to D2
  * Led2 connect to D3
  * Led3 connect to D4
- * Led4 connect to D5
- *
+
+
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -31,37 +31,33 @@
 const int pinLed1    = 2;                       // pin of led1
 const int pinLed2    = 3;                       // pin of led2
 const int pinLed3    = 4;                       // pin of led3
-const int pinLed4    = 5;                       // pin of led4
-
-
+ 
 void setup()
 {
     pinMode(pinLed1, OUTPUT);                   // set all pin OUTPUT
     pinMode(pinLed2, OUTPUT);
     pinMode(pinLed3, OUTPUT);
-    pinMode(pinLed4, OUTPUT);
 }
-
-
+ 
+ 
 void loop()
 {
     digitalWrite(pinLed4, LOW);                 // led4 off
     digitalWrite(pinLed1, HIGH);                // led1 on
-    
     delay(100);
-    
+ 
     digitalWrite(pinLed1, LOW);                 // led1 off
     digitalWrite(pinLed2, HIGH);                // led2 on
-    
+ 
     delay(100);
-    
+ 
     digitalWrite(pinLed2, LOW);                 // led2 off
     digitalWrite(pinLed3, HIGH);                // led3 on
-    
+ 
     delay(100);
-    
+ 
     digitalWrite(pinLed3, LOW);                 // led3 off
     digitalWrite(pinLed4, HIGH);                // led4 on
-    
+ 
     delay(100);
 }
